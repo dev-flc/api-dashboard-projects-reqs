57198988214ad1bb596b064829043e903ede2353
Fernando.LC <fer.lc.developer@gmail.com>
2022-10-05 22:18:09 -0500

    feat(express): config express and example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const example = () => console.log('example');
+import express from 'express';
 
-example();
+const PORT = process.env.PORT || 3002;
+const APP = express();
+
+APP.get('*', (request, response) => {
+  response.send(' W E L C O M E');
+});
+
+APP.listen(PORT, () => console.log(`Server, http://localhost:${PORT}`));

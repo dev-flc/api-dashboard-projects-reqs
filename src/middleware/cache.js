f96228d7136b22ae3d5f7785a85802fe87ec1b80
Fernando.LC <fer.lc.developer@gmail.com>
2022-11-05 20:22:26 -0600

    fix(cache): remove cache connect redis

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -1,16 +1,18 @@
-import ExpeditiousEngine from 'expeditious-engine-redis'
+// import ExpeditiousEngine from 'expeditious-engine-redis'
 import ExpressExpeditious from 'express-expeditious'
 
-const { REDIS_HOST, REDIS_PORT } = process.env
+// const { REDIS_HOST, REDIS_PORT } = process.env
 
+/*
 const redisOptions = {
   host: REDIS_HOST,
   port: REDIS_PORT
 }
+*/
 
 export const CACHE = ExpressExpeditious({
   defaultTtl: '1 minute',
-  engine: ExpeditiousEngine({ redis: redisOptions }),
+  //  engine: ExpeditiousEngine({ redis: redisOptions }),
   namespace: 'exampleCahe',
   statusCodeExpires: {
     404: '5 minutes',
